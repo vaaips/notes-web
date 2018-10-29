171b52f3b4f65fe86065d0e87cd9038671f10a0f
rahulspace <rahultp518@gmail.com>
2018-10-29 14:26:22 +0530

    Create and style header

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="home">
+    <div class="header">
+      <h2 class="heading">Notes</h2>
+    </div>
   </div>
 </template>
 
@@ -8,3 +11,21 @@ export default {
   name: 'home'
 }
 </script>
+
+<style lang="scss">
+  .home {
+    .header {
+      background: #f2b951;
+      height: 70px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      .heading {
+        font-size: 35px;
+        margin: 0;
+      }
+    }
+  }
+</style>
+
