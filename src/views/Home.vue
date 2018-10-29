4ca36ce80d49aacd4f282d5493f8ce281d688b83
rahulspace <rahultp518@gmail.com>
2018-10-29 15:59:32 +0530

    Create and style notes section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,21 @@
     <div class="header">
       <h2 class="heading">Notes</h2>
     </div>
+    <div class="main">
+      <div class="notes">
+        <div class="notes-wrapper">
+          <ul>
+            <li v-for="item of [1,2,3]">
+              <h4 class="note-title">Hello world!! fdgsdg sgfsddff mo</h4>
+              <p class="time">2 minutes ago</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="note">
+        
+      </div>
+    </div>
   </div>
 </template>
 
@@ -23,7 +38,52 @@ export default {
 
       .heading {
         font-size: 35px;
-        margin: 0;
+      }
+    }
+
+    .main {
+      display: flex;
+      height: calc(100vh - 70px);
+
+      .notes {
+        width: 30vw;
+        border-right: 1px solid #ddd;
+
+        .notes-wrapper {
+          ul {
+            list-style: none;
+            padding: 0;
+            margin: 0;
+
+            li {
+              border: 1px solid;
+              border-image: linear-gradient(to right, #fff, #fbc02d, #fff) !important;
+              border-image-slice: 1 !important;
+              border-top: none;
+              border-right: none;
+              border-left: none;
+              padding: 5px 10px;
+
+              .note-title {
+                font-size: 20px;
+                white-space: nowrap;
+                overflow: hidden;
+                text-overflow: ellipsis;
+                margin-bottom: 2px;
+              }
+
+              .time {
+                font-size: 15px;
+                color: #717171;
+              }
+            }
+          }
+        }
+      }
+
+      .note {
+        background: #efefef;
+        width: 70vw;
       }
     }
   }
