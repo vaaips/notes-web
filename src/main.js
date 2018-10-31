aa0558e691f41ed90af60f0e710665110819f77c
rahulspace <rahultp518@gmail.com>
2018-10-31 17:34:46 +0530

    Install and import lodash globally available on component templates

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
 import moment from 'moment'
+import _ from 'lodash'
 
 Vue.prototype.moment = moment
+Vue.prototype._ = _
 Vue.config.productionTip = false
 
 new Vue({
